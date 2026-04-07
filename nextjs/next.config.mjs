1cf6a27797afbd6cc9330d5d4683dc6b27948461
Akhilma2005 <maakhil432005@gmail.com>
2026-04-07 17:29:21 +0530

    push: latest changes

--- nextjs/next.config.mjs
+++ nextjs/next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    browsersListForSwc: true,
-  },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://pdf-tools-bzm0.onrender.com/api',
   },
