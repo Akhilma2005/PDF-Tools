61086edc36c5ba8e47af7106e0ac6c9a86e9a15b
Akhilma2005 <maakhil432005@gmail.com>
2026-04-07 17:16:46 +0530

    perf: fix desktop CLS, reduce transform distances, preload font

--- nextjs/pages/_document.js
+++ nextjs/pages/_document.js
@@ -15,6 +15,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" media="print" onLoad="this.media='all'" />
+        <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" />
         <noscript>
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
         </noscript>
